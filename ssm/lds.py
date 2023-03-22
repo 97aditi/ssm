9ec19e37e35d1b94851f3bf4cee7eeb91f172e76
Aditi Jha <aditijha@princeton.edu>
2023-03-22 12:52:37 -0400

    modified total data ll

--- ssm/lds.py
+++ ssm/lds.py
@@ -697,7 +697,7 @@ class SLDS(object):
                                 optimizer=emission_optimizer,
                                 maxiter=emission_optimizer_maxiter, emission_block_diagonal=emission_block_diagonal,
                                 dynamics_dales_constraint = dynamics_dales_constraint,)
-            # self.emissions.params = convex_combination(curr_prms, self.emissions.params, alpha)
+            self.emissions.params = convex_combination(curr_prms, self.emissions.params, alpha)
 
     def _laplace_em_elbo(self,
                          variational_posterior,
@@ -732,7 +732,7 @@ class SLDS(object):
                     exp_log_joint += np.sum(Ez[0] * log_pi0)
                     exp_log_joint += np.sum(Ezzp1 * log_Ps)
                     exp_log_joint += np.sum(Ez * log_likes)
-            return exp_log_joint / n_samples
+            return exp_log_joint / (n_samples)
         
         def estimate_complete_data_log_likelihood():
             """ Estimate the complete data log likelihood for Gaussian LDS"""
@@ -756,40 +756,39 @@ class SLDS(object):
             Q_inv = np.linalg.inv(Q)
             R_inv = np.diag(1/np.exp(inv_etas[0]))
 
-            # first initialize the sufficient statistics for the continuous states
-            M_delta = np.zeros((self.D+1, self.D))
-            Y = np.zeros((self.N, self.N))
-            Y_tilde = np.zeros((self.D+1, self.N))
-            M_2T = np.zeros((self.D, self.D))
-            M_1T = np.zeros((self.D+1, self.D+1))
-            M_1T_1 = np.zeros((self.D+1, self.D+1))
-
             ll_dynamics = 0
             ll_observations = 0
 
-            # now ompute them usin smoothed means and covariances
+            # now compute them using smoothed means and covariances
             continuous_expectations = variational_posterior.continuous_expectations
             for (_, Ex, smoothed_sigmas, ExxnT), data in zip(continuous_expectations, datas):
 
-                _, Ex, smoothed_sigmas, ExxnT = variational_posterior.continuous_expectations[0]
-                M_delta[:self.D, :self.D] += np.sum(ExxnT, axis=0)
-                M_delta[self.D, :self.D] += np.sum(Ex[1:], axis=0)
+                # first initialize the sufficient statistics for the continuous states
+                M_delta = np.zeros((self.D+1, self.D))
+                Y = np.zeros((self.N, self.N))
+                Y_tilde = np.zeros((self.D+1, self.N))
+                M_2T = np.zeros((self.D, self.D))
+                M_1T = np.zeros((self.D+1, self.D+1))
+                M_1T_1 = np.zeros((self.D+1, self.D+1))
 
-                Y += np.sum(np.einsum('ti,tj->tij',data, data), axis=0)
-                Y_tilde[:self.D,:] += np.sum(np.einsum('ti,tj->tij',Ex, data), axis=0)
-                Y_tilde[self.D,:] += np.sum(data, axis=0)
+                M_delta[:self.D, :self.D] = np.sum(ExxnT, axis=0)
+                M_delta[self.D, :self.D] = np.sum(Ex[1:], axis=0)
+
+                Y = np.sum(np.einsum('ti,tj->tij',data, data), axis=0)
+                Y_tilde[:self.D,:] = np.sum(np.einsum('ti,tj->tij',Ex, data), axis=0)
+                Y_tilde[self.D,:] = np.sum(data, axis=0)
             
                 mumuT = np.einsum('ti,tj->tij',Ex, Ex) + smoothed_sigmas
-                M_2T += np.sum(mumuT[1:], axis=0)
-                M_1T_1[:self.D,:self.D] += np.sum(mumuT[:-1], axis=0)
-                M_1T_1[self.D,:self.D] += np.sum(Ex[:-1], axis=0)
-                M_1T_1[:self.D,self.D] += np.sum(Ex[:-1], axis=0)
-                M_1T_1[self.D,self.D] += data.shape[0]-1
+                M_2T = np.sum(mumuT[1:], axis=0)
+                M_1T_1[:self.D,:self.D] = np.sum(mumuT[:-1], axis=0)
+                M_1T_1[self.D,:self.D] = np.sum(Ex[:-1], axis=0)
+                M_1T_1[:self.D,self.D] = np.sum(Ex[:-1], axis=0)
+                M_1T_1[self.D,self.D] = data.shape[0]-1
 
-                M_1T[:self.D,:self.D] += np.sum(mumuT, axis=0)
-                M_1T[self.D,:self.D] += np.sum(Ex, axis=0)
-                M_1T[:self.D,self.D] += np.sum(Ex, axis=0)
-                M_1T[self.D,self.D] += data.shape[0]
+                M_1T[:self.D,:self.D] = np.sum(mumuT, axis=0)
+                M_1T[self.D,:self.D] = np.sum(Ex, axis=0)
+                M_1T[:self.D,self.D] = np.sum(Ex, axis=0)
+                M_1T[self.D,self.D] = data.shape[0]
 
                 # now compute the log likelihood
                 ll_dynamics += -0.5*np.trace(Q_inv@M_2T) + np.trace(Q_inv@A@M_delta) -0.5*np.trace(Q_inv@A@M_1T_1@A.T) \
@@ -1108,7 +1107,6 @@ class LDS(SLDS):
             # accounting for observation bias by subtracting ds[0]
             ll_this, _, _ = kalman_filter(mu0, S0, As[0], Vs[0], Q, Cs[0], Fs[0], R, input, data-ds[0])
             ll += ll_this
-        ll = ll/len(datas)
         
         return ll + self.log_prior()
 
