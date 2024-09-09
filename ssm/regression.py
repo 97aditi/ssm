3399736f8f90eae7d5d6a24aaaa037396f6a8c74
97aditi <aditidps07@gmail.com>
2024-09-09 18:22:21 -0400

    added multi-region code back in

--- ssm/regression.py
+++ ssm/regression.py
@@ -279,6 +279,39 @@ def solve_neuron_wise_regression_for_C(ExxT, ExyT, fit_intercept, initial_C, eta
     return W
 
 
+def solve_multi_region_lds_regression(ExxT, ExyT, region_identity, list_of_dims, prior_ExxT):
+    """ solve regression in case of LDS model with separate latents per region but no other constraints """
+
+    # get expectations
+    ExxT = ExxT + prior_ExxT
+    # separate the emission matrix for each region
+    num_neurons = ExyT.shape[1]
+    W_full = np.zeros((num_neurons, ExxT.shape[0]))
+    region_ids = np.unique(region_identity).astype(int)
+
+    assert list_of_dims[0]==list_of_dims[1], "multi-region LDS code currently cannot handle different latents per region, should fix soon"
+
+    for region_id in region_ids:
+        latent_space_dim = list_of_dims[region_id]
+        idx_neurons_this_region = np.where(region_identity==region_id)[0]
+        ExxT_region = ExxT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, region_id*latent_space_dim:(region_id+1)*latent_space_dim]
+
+        if fit_intercept:
+            ExxT_region = np.hstack((ExxT_region, ExxT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, -1][:,None]))
+            last_row = np.zeros(latent_space_dim+1)
+            last_row[-1], last_row[:latent_space_dim] = ExxT[-1, -1], ExxT[-1, region_id*latent_space_dim:(region_id+1)*latent_space_dim]
+            ExxT_region = np.vstack((ExxT_region, last_row[None,:]))
+        ExyT_region = ExyT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, idx_neurons_this_region]
+        if fit_intercept:
+            ExyT_region = np.concatenate((ExyT_region, [ExyT[-1, idx_neurons_this_region]]))
+
+        solution = np.linalg.solve(ExxT_region, ExyT_region).T
+        W_full[idx_neurons_this_region, region_id*latent_space_dim:(region_id+1)*latent_space_dim] = solution[:, :latent_space_dim]
+        if fit_intercept:
+            W_full[idx_neurons_this_region, -1] = solution[:, -1]   
+    return W_full
+
+
 def fit_linear_regression(Xs, ys,
                           weights=None,
                           fit_intercept=True,
@@ -374,30 +407,11 @@ def fit_linear_regression(Xs, ys,
         # update C
         ExxT = ExxT + prior_ExxT
         W_full = solve_neuron_wise_regression_for_C(ExxT, ExyT, fit_intercept, initial_C, \
-                    current_etas, dynamics_dales_constraint, infer_sign, latent_space_dim, region_identity)
-    # check if there are multiple region identities
-    elif len(np.unique(region_identity))>1:
-        ExxT = ExxT + prior_ExxT
-        # separate the emission matrix for each region
-        num_neurons = ExyT.shape[1]
-        W_full = np.zeros((num_neurons, ExxT.shape[0]))
-        region_ids = np.unique(region_identity).astype(int)
-        for region_id in region_ids:
-            idx_neurons_this_region = np.where(region_identity==region_id)[0]
-            ExxT_region = ExxT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, region_id*latent_space_dim:(region_id+1)*latent_space_dim]
-            if fit_intercept:
-                ExxT_region = np.hstack((ExxT_region, ExxT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, -1][:,None]))
-                last_row = np.zeros(latent_space_dim+1)
-                last_row[-1], last_row[:latent_space_dim] = ExxT[-1, -1], ExxT[-1, region_id*latent_space_dim:(region_id+1)*latent_space_dim]
-                ExxT_region = np.vstack((ExxT_region, last_row[None,:]))
-            ExyT_region = ExyT[region_id*latent_space_dim:(region_id+1)*latent_space_dim, idx_neurons_this_region]
-            if fit_intercept:
-                ExyT_region = np.concatenate((ExyT_region, [ExyT[-1, idx_neurons_this_region]]))
+                    current_etas, dynamics_dales_constraint, list_of_dims, infer_sign, latent_space_dim, region_identity)
 
-            solution = np.linalg.solve(ExxT_region, ExyT_region).T
-            W_full[idx_neurons_this_region, region_id*latent_space_dim:(region_id+1)*latent_space_dim] = solution[:, :latent_space_dim]
-            if fit_intercept:
-                W_full[idx_neurons_this_region, -1] = solution[:, -1]   
+    # check if there are multiple region identities, but no Dale's constraint
+    elif len(np.unique(region_identity))>1:
+        W_full = solve_multi_region_lds_regression(ExxT, ExyT, region_identity, list_of_dims, prior_ExxT)
     else:
         W_full = np.linalg.solve(ExxT, ExyT).T
         
